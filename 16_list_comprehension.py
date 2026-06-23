48e9e3405a6176b38701901b79aec15e68bf41a3
CodeWithAyush <ayush0712pandey@gmail.com>
2026-06-23 19:42:14 +0530

    Rivision and learning more about python
    Before this commit I have learned abhot list comprehension in more detail.

--- 16_list_comprehension.py
+++ 16_list_comprehension.py
@@ -14,3 +14,41 @@ print(t)
 square_table = [i**2 for i in range(1,11)]
 print(square_table)
 
+
+"""Using for and if in list comprehension"""
+#Create a list of numbers which is multiple of 3 and divisible of 2 between 1 to 100
+list2 = [3*i for i in range (1, 35) if ((3*i)<100) and ((3*i)%2==0) ]
+print(list2)
+
+
+"""Using for, if and else in list comprehension"""
+list3 = ["Even" if i%2 == 0 else "Odd" for i in range (1, 30)]
+print(list3)
+
+
+"""Creating pairs usinf nested for loop"""
+list4 = [(x, y) for x in range(2) for y in range(4)]
+print(list4)
+
+# for i in range (2):
+#     for j in range(4):
+#         print((i,j))
+
+
+"""list comprehension with function"""
+animal = ["Tiger", "Lion", "Dog", "Cat", "Cow", "Horse"]
+list5 = [item.upper() for item in animal]
+print(list5)
+
+
+""" List Comprehension with Nested List"""
+matrix = [[1, 2], [3, 4], [5, 6]]
+print(matrix)
+print([item for row in matrix for item in row])
+
+
+"""List Comprehension with Set and Dictionary
+Comprehensions"""
+#Set Comprehension
+unique_no = { x for x in [1, 2, 3, 3, 5, 4, 2, 4, 3]}
+print(unique_no)
--- 19_dictionary.py
+++ 19_dictionary.py
@@ -17,3 +17,17 @@ print(toped_on.values())
 print(toped_on.items())
 toped_on.pop("English")
 print(toped_on)
+
+list1 = [toped_on.get("Maths", 0), toped_on.get("English", 0)]
+print(list1) #.get('key', default_value) gives the value of key if found and if not then returns the defalut value,
+toped_on.update({"Hindi" : "Stephen Hawkin"})
+print(toped_on)
+toped_on["English"] = "Kishor Kumar"
+print(toped_on)
+print(toped_on.pop("hduh", 0)) #Here if any key is not available in the dict then it will not throw an error insted retuen the default value
+
+new_toped_on = toped_on.copy()
+print(new_toped_on)
+
+toped_on.clear()
+print(toped_on)
